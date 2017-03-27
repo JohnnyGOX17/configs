168d4b4825f529fa48f1a1c603e3b63a801ec30e
nitest <noemail@ni.com>
2017-03-27 18:45:38 -0500

    add update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# 
+# Shell script to update all needed development tools
+
+cp ./configs/.bashrc ~/.bashrc
+cp ./configs/.vimrc ~/.vimrc
+cp ./configs/gmonokai.vim ~/.vim/colors/gmonokai.vim
