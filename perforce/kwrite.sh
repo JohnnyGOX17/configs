e6edb90a26341457510f26034e993af5a9563857
Gentile, John <johncgentile17@gmail.com>
2019-04-02 11:18:56 -0400

    fixed up git scripts and reorganized

--- perforce/kwrite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# File              : kwrite.sh
-# Author            : John Gentile <johncgentile17@gmail.com>
-# Date              : 14.02.2018
-# Last Modified Date: 14.02.2018
-# Last Modified By  : John Gentile <johncgentile17@gmail.com>
-LD_LIBRARY_PATH=/home/jgentile/Perforce/lib/icu
-export LD_LIBRARY_PATH
-kwrite $1
