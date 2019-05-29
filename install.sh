3b1ce27d18227fe0bf39acfab798dcf1d78b097c
Gentile, John <johncgentile17@gmail.com>
2019-05-29 16:15:52 -0400

    script updates for git operations

--- install.sh
+++ install.sh
@@ -2,7 +2,14 @@
 
 insLoc=/usr/local/bin/
 
-cp ./dev_utils/git_pulls        $insLoc
-cp ./dev_utils/git_statuses     $insLoc
-cp ./sys_utils/scrot-screenshot $insLoc
-cp ./sys_utils/tmux_mem_cpu     $insLoc
+cp ./dev_utils/generate-sitemap          $insLoc
+cp ./dev_utils/git/git-print-TODOs       $insLoc
+cp ./dev_utils/git/git-rewrite-author    $insLoc
+cp ./dev_utils/git/git-status-src-dirs   $insLoc
+cp ./dev_utils/git/git-update-src-dirs   $insLoc
+cp ./dev_utils/git/git-rm-dos-whitespace $insLoc
+
+cp ./sys_utils/scrot-screenshot          $insLoc
+cp ./sys_utils/tmux_mem_cpu              $insLoc
+
+echo "Scripts installed to $insLoc"
