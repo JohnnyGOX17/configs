a9e90faee80db266b4691a2f7cb8724e38f80e3f
Gentile, John <johncgentile17@gmail.com>
2021-08-03 10:42:07 -0400

    bat stuff

--- copy_vim_windows.bat
+++ copy_vim_windows.bat
@@ -7,6 +7,7 @@
 :: After install, re-run install.py
 :: Install PowerLine fonts https://medium.com/@slmeng/how-to-install-powerline-fonts-in-windows-b2eedecace58
 :: Make sure swap directories (i.e. C:\temp\vim) exist
+::
 
 xcopy configs\_gvimrc "C:\Program Files (x86)\Vim\_gvimrc" /D /H /R /Y /K
 xcopy configs\_vimrc "C:\Program Files (x86)\Vim\_vimrc" /D /H /R /Y /K
