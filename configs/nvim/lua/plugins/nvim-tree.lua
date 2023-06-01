2d232cfa90db36d75f93830d5dcba3dc27f977d2
Gentile, John <johncgentile17@gmail.com>
2023-06-01 16:12:55 -0400

    update nvim-tree to new key mappings for on_attach

--- configs/nvim/lua/plugins/nvim-tree.lua
+++ configs/nvim/lua/plugins/nvim-tree.lua
@@ -6,15 +6,20 @@ return {
     require("nvim-tree").setup({
       view = {
         adaptive_size = true,
-        mappings = {
-          list = {
-            { key = "h", action = "close_node" },
-            { key = "l", action = "edit" },
-          },
-        },
       },
+      on_attach = function(bufnr)
+        local api = require('nvim-tree.api')
+
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+        vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+      end,
     })
 
     vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { desc = 'Open nvim-tree file viewer on the left', silent = true })
   end
 }
+
