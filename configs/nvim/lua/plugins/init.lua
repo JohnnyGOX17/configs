b94cc0a2644cbdeb05966f265c5757a39b3630e6
John Gentile <johncgentile17@gmail.com>
2026-07-29 16:44:08 -0400

    Update init.lua
    remove old treesitter config

--- configs/nvim/lua/plugins/init.lua
+++ configs/nvim/lua/plugins/init.lua
@@ -1,97 +1,44 @@
 return { -- Configure treesitter here, other plugins in other lua files
-  { -- Highlight, edit, and navigate code
+  {
+    -- NOTE: may need to install tree-sitter-cli like `cargo install --locked tree-sitter-cli`
     'nvim-treesitter/nvim-treesitter',
-    branch = "master",
-    version = false,
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    dependencies = {
-      -- Additional text objects via treesitter 
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
     config = function()
-      -- [[ Configure Treesitter ]]
-      -- See `:help nvim-treesitter`
-      require('nvim-treesitter.configs').setup {
-        -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = {
-          'bash',
-          'c',
-          'cpp',
-          'cuda',
-          'dockerfile',
-          'go',
-          'html',
-          'java',
-          'javascript',
-          'lua',
-          'markdown',
-          'markdown_inline',
-          'python',
-          'regex',
-          'rust',
-          'typescript',
-          'verilog',
-          'vim',
-          'vimdoc'
-        },
-
-        -- Don't highlight for Markdown as preservim/vim-markdown handles other concealment
-        highlight = { enable = true, disable = { 'markdown' } },
-        indent = { enable = true, disable = { 'c', 'cpp', 'python', 'markdown' } },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<c-backspace>',
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[['] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[]'] = '@class.outer',
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<leader>a'] = '@parameter.inner',
-            },
-            swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
-            },
-          },
-        },
-      }
-    end
+      require('nvim-treesitter').install({
+        'bash',
+        'c',
+        'cpp',
+        'cuda',
+        'dockerfile',
+        'go',
+        'html',
+        'java',
+        'javascript',
+        'json',
+        'lua',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'regex',
+        'rust',
+        'tcl',
+        'typescript',
+        'vhdl',
+        'verilog',
+        'vim',
+        'vimdoc',
+        'yaml',
+      })
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(ev)
+          pcall(vim.treesitter.start, ev.buf)
+          vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
   },
 }
